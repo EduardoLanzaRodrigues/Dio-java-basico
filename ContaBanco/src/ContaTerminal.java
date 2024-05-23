7b898197baa837fff9c7e445bb46ac8e86afc191
EduardoLanzaRodrigues <136534675+eduardolanzarodrigues@users.noreply.github.com>
2024-05-23 10:51:20 -0300

    Initial commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,16 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente ="Mario Andrade";
+        double   saldo = 237.48;
+
+        System.out.println("Por favor digite o número da Agência! ");
+        System.out.println("1021");
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+
+    }
+}
+
