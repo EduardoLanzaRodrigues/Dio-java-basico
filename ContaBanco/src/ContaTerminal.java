4c25d248e68e581ba3d7128e742047b2b18dae38
eduardo <eduardolanza244@gmail.com>
2024-05-23 13:37:42 -0300

    Merge remote-tracking branch 'origin/main'

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        int numero = 1021;
+        int numeroDaConta = 1021;
         String agencia = "067-8";
+        String usuario = "1004";
         String nomeCliente ="Mario Andrade";
         double   saldo = 237.48;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da agência: ");
+        scanner.next();
+        System.out.println("Usuário: " + usuario);
 
-        System.out.println("Por favor digite o número da Agência! ");
-        System.out.println("1021");
-
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência  é "+agencia+", conta " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque");
 
 
     }
